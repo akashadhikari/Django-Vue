156c70f4d92445e3ee3715bf85d908d8e83b2bf8
Akash Adhikari <akash_532998@yahoo.com>
2017-12-25 20:47:59 +0545

    Pagination works

--- crm/settings.py
+++ crm/settings.py
@@ -107,7 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 1
 }
 
 # Internationalization
